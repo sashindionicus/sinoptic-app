634c85cfeed1fc158b5775694696b197d0a1ce95
sashindionicus <sashindionicus465@gmail.com>
2021-04-26 13:51:53 +0300

    Add all layout

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,16 +1,21 @@
-import { Card, Button } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 
 function InfoCard() {    
     return(
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card className="card shadow-lg rounded" style={{ width: '20.5rem' }}>
+        <Card.Img src="https://via.placeholder.com/400x300" className="time card-img-top"/>
         <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <div class="icon bg-light mx-auto text-center">
+                <img src="" alt=""/>
+            </div>   
+            <div class="text-muted text-uppercase text-center details">
+                <h5 class="my-3">Kharkiv</h5>
+                <div class="my-3">Sunny</div>
+                <div class="display-4 my-4">
+                    <span>13.9</span>
+                    <span>&deg;C</span>
+                </div>
+            </div>
         </Card.Body>
         </Card>
     )}
