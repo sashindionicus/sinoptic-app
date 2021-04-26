4fba886d7ce2e281638a181c49c9ad1f1a670dc4
sashindionicus <sashindionicus465@gmail.com>
2021-04-26 14:31:20 +0300

    Fix roots for img

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -3,10 +3,10 @@ import { Card } from "react-bootstrap"
 function InfoCard() {    
     return(
         <Card className="card shadow-lg rounded" style={{ width: '20.5rem' }}>
-        <Card.Img src="https://via.placeholder.com/400x300" className="time card-img-top"/>
+        <Card.Img src="img/es_day.jpg" className="time card-img-top"/>
         <Card.Body>
             <div className="icon bg-light mx-auto text-center">
-                <img src="" alt=""/>
+                <img src="img/icons/1.svg" alt=""/>
             </div>   
             <div className="text-muted text-uppercase text-center details">
                 <h5 className="my-3">Kharkiv</h5>
